023eb0fbf88238bd930e6c887ac8911a82727a3f
devebe <dvbirdja@gmail.com>
2022-08-16 22:46:34 +0200

    follow exercise

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const container = document.querySelector('#container');
+
 const p = document.createElement('p');
+p.textContent = "Hi, I'm red!";
 p.style.color = 'red';
-p.textContent = "Hey I'm red!";
+
+const h3 = document.createElement('h3');
+h3.textContent = "I'm a blue h3"
+h3.style.color = 'blue';
+
+const div = document.createElement('div');
+div.style.backgroundColor = 'pink';
+div.style.border = '1px solid black';
+
+const h1 = document.createElement('h1');
+h1.textContent = "I'm in a div";
+
+const divP = document.createElement('p');
+divP.textContent = "Me too!";
+
+container.appendChild(p);
+container.appendChild(h3);
+container.appendChild(div);
+div.appendChild(h1);
+div.appendChild(divP);
